6623a73c62e46c6020b96a06b353a50e5db461b9
jennifercardoza <jennifer.cardoza2026@adoc.superate.org>
2026-09-25 10:43:21 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const btnEstadisticas = document.getElementById("btnEstadisticas");
+const estadisticas = document.getElementById("estadisticas");
+
+btnEstadisticas.addEventListener("click", () => {
+    estadisticas.textContent = "Carreras: 10 | Puntos: 250";
+});
